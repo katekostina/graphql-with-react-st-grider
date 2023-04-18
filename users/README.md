f93711ae519480b98bedca7615dffac0b481fed5
Kate Kostina <katekostina@yahoo.com>
2023-04-19 01:18:56 +0400

    Refactor folders. Add readme. Add database.

---
+++ users/README.md
@@ -0,0 +1,10 @@
+to start a database:
+`npm run json:server`
+
+to start a server:
+`npm run dev` 
+// with nodemon
+
+// or just
+`npm run start`
+
--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const { 
   GraphQLObjectType, 
   GraphQLString, 
   GraphQLInt, 
   GraphQLSchema,
 } = graphql;
-
-const users = [
-  {id: '42', firstName: 'Katie', age: 29},
-  {id: '23', firstName: 'Evgeniia', age: 30},
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +23,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data);
       }
     }
 
